9e5787c13c283bb5695c4d50b9ef0d96c5789298
OneYoungMean <3303609008@qq.com>
2022-05-13 21:49:47 +0800

    提交测试场景

--- Assets/MagicMotion/Runtime/Mono/IGetConstraint.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace MagicMotion.Mono
-{
-    internal interface IGetConstraint
-    {
-        public MMConstraint GetConstraintTarget(HumanBodyBones humanBodyBones, MMConstraintType constraintType);
-    }
-}
